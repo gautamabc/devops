2b9181a5fea9c4c222f815e3f3ba0515fed26d8b
Gautam Singh <146442536+gautamabc@users.noreply.github.com>
2026-03-12 14:36:26 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,8 @@
 public class HelloWorld {
 
     public static void main(String[] args) {
-        System.out.println("Hello from Jenkins Pipeline");
+        System.out.println("Hello this is tomcat");
     }
+
 
 }
